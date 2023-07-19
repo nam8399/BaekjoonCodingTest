6c2be39a7152891dfecfffa1fbe72b2ad02f050d
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-07-19 20:47:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.kt
@@ -0,0 +1,10 @@
+import java.io.*
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val str : Char = readLine().first()
+
+
+    println(str.toInt())   
+}
