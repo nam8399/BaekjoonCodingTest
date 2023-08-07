7151c2c9690cd9c4676700f52ea2a1d6e2d7a429
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-08-07 16:43:46 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1424 ms, Memory: 123828 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -2,8 +2,12 @@ import java.io.*
 import java.util.*
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val wrt = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
     val arr = Array(n) {readLine().split(" ").toList()}.sortedWith(compareBy({it.get(0).toInt()},{it.get(1).toInt()}))
     
-    for (i in 0 until n) println(arr[i].get(0) + " " + arr[i].get(1))
+    for (i in 0 until n) wrt.write(arr[i].get(0) + " " + arr[i].get(1) + "\n")
+    
+    wrt.flush()
+    wrt.close()
 }
