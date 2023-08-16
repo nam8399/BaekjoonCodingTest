a35837df3d2fb658832a0823c318f15ec188e90d
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-08-16 14:25:08 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 12636 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.kt
+++ 백준/Silver/7568. 덩치/덩치.kt
@@ -4,8 +4,6 @@ import java.util.*
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val n = readLine().toInt()
     var list = mutableListOf<Pair<Int, Int>>()
-    var wList = mutableListOf<Int>()
-    var map = mutableMapOf<Int, Int>()
     var result = ""
     
     repeat(n) {
@@ -15,9 +13,8 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     
     for (i in 0 until n) {
         var fCnt = list.filter {it.first > list.get(i).first && it.second > list.get(i).second}.count()+1
-        var sCnt = list.filter {it.first >= list.get(i).first && it.second >= list.get(i).second}.count()+1
         result += "$fCnt "
     }
     
-    println(result.substring(0,result.length-1))
+    println(result)
 }
