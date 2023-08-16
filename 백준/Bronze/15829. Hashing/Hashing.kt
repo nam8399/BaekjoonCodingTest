e9547da44f16332a04f081debe8d223df5725737
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-08-16 20:27:00 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 12172 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -0,0 +1,28 @@
+import java.io.*
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    
+    var str = readLine()
+    var result = 0
+    var cnt = 0
+    
+    str.forEach {
+        result += (it.toInt()-96) * calculate(cnt)
+        cnt ++
+    }
+    
+    print(result)
+    
+	
+}
+
+fun calculate(n : Int) : Int {
+    var result = 1
+    repeat(n) {
+    	result *= 31    
+    }
+    
+    return result
+}
