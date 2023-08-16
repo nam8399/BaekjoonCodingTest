ae04f5589c51779d46bfeb3d0d2a0616acfa91b1
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-08-16 18:01:56 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2608 ms, Memory: 339920 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -3,7 +3,6 @@ import java.util.*
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val wrt = BufferedWriter(OutputStreamWriter(System.out))
-    var sb = StringBuilder("")
     val n = readLine().toInt()
     var list = IntArray(n) {0}
     
@@ -12,8 +11,6 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     list.sort()
     
     for (i in 0 until n) wrt.write(list.get(i).toString() + "\n")
-    
-    wrt.write(sb.toString())
     
     wrt.flush()
     wrt.close()
