ddc528c7a2dfe4f70375e8222290be5fb7350bc9
nam8399 <69960282+nam8399@users.noreply.github.com>
2023-08-22 10:55:24 +0900

    [Silver II] Title: 마인크래프트, Time: 772 ms, Memory: 46008 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.kt
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.kt
@@ -2,14 +2,12 @@ import java.io.*
 import java.util.*
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var wrt = BufferedWriter(OutputStreamWriter(System.out))
-    var (n, m, b) = readLine().split(" ").map { it.toInt() }
+    var (n, m, b) = readLine().split(" ").map {it.toInt()}
     
     var arr = Array<Array<Int>>(n) { Array<Int>(m) {0} }
-    
     var min = Int.MAX_VALUE
-    var height = 0
     var sum = 0
+    var height = 0
     
     for (i in 0 until n) {
         val str = StringTokenizer(readLine())
@@ -19,25 +17,23 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         }
     }
     
-    height = if((sum + b)/(n*m) >= 256) 256 else (sum + b)/(n*m)
+    var max = if ((sum+b)/(n*m) > 256) 256 else (sum+b)/(n*m)
     
-    for (i in 0 .. height) {
-        var time = 0
+    for (i in 0 .. max) {
+        var cnt = 0
         for (j in 0 until n) {
             for (k in 0 until m) {
-                if (arr[j][k] < i) time += i-arr[j][k]
-                else if (arr[j][k] > i) time += (arr[j][k]-i)*2
+                if (i > arr[j][k]) cnt += i-arr[j][k]
+                else if (arr[j][k] > i) cnt += (arr[j][k]-i)*2
             }
         }
         
-        if (min >= time) {
-            min = time
+        if (cnt <= min) {
+            min = cnt
             height = i
         }
     }
     
-    wrt.write(min.toString() + " " + height.toString())
+    println("$min $height")
     
-    wrt.flush()
-    wrt.close()
 }
